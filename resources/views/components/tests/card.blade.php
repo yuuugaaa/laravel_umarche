7949c11442eb61e830c564e4ac364400a374dcdd
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-10 22:33:47 +0900

    属性バッグテスト

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,9 @@
     'message' => '初期値メッセージ'
 ])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/tests/component1.blade.php
+++ resources/views/tests/component1.blade.php
@@ -7,4 +7,5 @@
 
     <x-tests.card title="タイトル1" content="コンテンツ1" :message=$message />
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSの変更" class="bg-red-300" />
 </x-tests.app>
