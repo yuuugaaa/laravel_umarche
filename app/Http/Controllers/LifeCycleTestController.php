af2429da5e766c074a7bc6bb69dbbbdcae4df4bc
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-11 11:47:46 +0900

    サービスコンテナテスト

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainer()
+    {
+        app()->bind('lifeCycleTest', function() {
+            return 'ライフサイクルのテストです。';
+        });
+
+        $test = app()->make('lifeCycleTest');
+
+        //サービスコンテナなし
+        //$message = new Message();
+        //$sample = new Sample($message);
+        //$sample->run();
+
+        //サービスコンテナあり
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    public function run()
+    {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo 'メッセージ表示';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LifeCycleTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/servicecontainer', [LifeCycleTestController::class, 'showServiceContainer']);
 
 require __DIR__.'/auth.php';
