ef4ce4e6c5030b9c56f490c8e2474363dd3668d6
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-15 10:06:32 +0900

    Shopの追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Shop;
 
 class Owner extends Authenticatable
 {
@@ -41,4 +42,9 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function shop()
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
