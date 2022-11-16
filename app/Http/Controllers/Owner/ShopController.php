4748637cc00f99d3d6e80c590c337af6a67a33b3
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-17 01:15:35 +0900

    index画面

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -32,8 +32,7 @@ class ShopController extends Controller
 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view('owner.shops.index', compact('shops'));
     }
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            店舗情報
         </h2>
     </x-slot>
 
@@ -9,9 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach ($shops as $shop)
-                        {{ $shop->name }}
-                    @endforeach
+                    <section class="text-gray-600 body-font">
+                        <div class="container px-5 py-4 mx-auto">
+                        <div class="flex flex-wrap -m-4">
+                            @foreach ($shops as $shop)
+                                <div class="p-4 md:w-1/3">
+                                <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}" class="block h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                                    @if ($shop->filename)
+                                        <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset('storage/shops'. $shop->filename) }}" alt="shop">
+                                    @else
+                                        <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset('images/no_image.jpg') }}" alt="no image">
+                                    @endif
+                                    <div class="p-4">
+                                        @if ($shop->is_selling)
+                                            <span class="text-green-400 tracking-widest text-xs title-font font-medium mb-1">販売中</span>
+                                        @else
+                                            <span class="text-red-400 tracking-widest text-xs title-font font-medium mb-1">停止中</span>
+                                        @endif
+                                        <h3 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $shop->name }}</h3>
+                                        <p class="leading-relaxed mb-3">{{ $shop->information }}</p>
+                                    </div>
+                                </a>
+                                </div>
+                            @endforeach
+                        </div>
+                        </div>
+                    </section>
+
                 </div>
             </div>
         </div>
