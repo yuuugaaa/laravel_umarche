a0490940bd946bd17bc2e8ed609cdc955251c65a
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-21 11:00:53 +0900

    imageのdestroy作成

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,7 @@ use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -124,6 +125,20 @@ class ImageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/'. $image->filename;
+
+        if (Storage::exists($filePath)) {
+            Storage::delete($filePath);
+        }
+
+        $image->delete();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with([
+            'message' => '画像を削除しました。',
+            'status' => 'alert',
+        ]);
     }
 }
