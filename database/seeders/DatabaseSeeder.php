355cba6e171e8da6455eea46fe7871098a8ca37d
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-21 11:35:08 +0900

    ImageSeeder作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
