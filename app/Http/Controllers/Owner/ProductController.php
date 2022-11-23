5c76d9377771b6ae77c2b46b112f1fda405700a5
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-23 23:20:07 +0900

    productのcreate作成（category）

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use App\Models\Owner;
+use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
+use App\Models\Shop;
 
 class ProductController extends Controller
 {
@@ -46,7 +49,19 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+        ->select('id','name')
+        ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+        ->select('id', 'filename', 'title')
+        ->orderBy('updated_at', 'desc')
+        ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
+        return view('owner.products.create', compact('shops', 'images', 'categories'));
     }
 
     /**
