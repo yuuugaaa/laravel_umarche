9d881e671f8020691da744e87dc1e64f7f1c0991
yuuugaaa <yuuugaaa.321@gmail.com>
2022-11-30 18:30:38 +0900

    stripe controller側の設定

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -51,4 +51,36 @@ class CartController extends Controller
 
         return redirect()->route('user.cart.index');
     }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $lineItems = [];
+
+        foreach ($products as $product) {
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+
+            array_push($lineItems, $lineItem);
+        }
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', compact('checkout_session', 'publicKey'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::middleware('auth:users')->group(function () {
             Route::get('/', [CartController::class, 'index'])->name('cart.index');
             Route::post('add', [CartController::class, 'add'])->name('cart.add');
             Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+            Route::get('checkout', [CartController::class, 'checkout'])->name('cart.checkout');
         });
     });
 
